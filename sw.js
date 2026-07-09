24d6e608d824382c38f8f8ed2cffdec7c8e26e0d
kdwcoc1-kor <kdwcoc1@gmail.com>
2026-07-09 18:45:16 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    공지/행사 내용을 수정해서 새로 배포할 때마다
    아래 버전 숫자(v1 → v2 → v3 ...)를 하나씩 올려 주세요.
    그래야 회원들 휴대폰에 새 내용이 반영됩니다. */
-const CACHE = "goheung-app-v2";
+const CACHE = "goheung-app-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
 
 self.addEventListener("install", e => {
